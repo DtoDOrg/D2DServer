a1a4b1396a52e2238cac1429a6427e20942c7462
Jayanta <=>
2024-10-27 16:24:11 +0530

    image kit modification

--- helper/imageUploader.js
+++ helper/imageUploader.js
@@ -1,24 +1,23 @@
-import { CONFIG } from "../config/config.js";
-import ImageKit from "imagekit";
+import { CONFIG } from '../config/config.js';
+import ImageKit from 'imagekit';
 var imagekit = new ImageKit({
-  publicKey: CONFIG.IMAGEKIT_PUB,
-  privateKey: CONFIG.IMAGEKIT_PRI,
-  urlEndpoint: `https://ik.imagekit.io/${CONFIG.IMAGE_KIT_ID}`,
+    publicKey: CONFIG.IMAGE_KIT_PUB,
+    privateKey: CONFIG.IMAGE_KIT_PRI,
+    urlEndpoint: `https://ik.imagekit.io/${CONFIG.IMAGE_KIT_ID}`,
 });
-export const ImageUploader = async (file, folder) => {
-  try {
-    // id = gvspmkmsw;
-    const details = await imagekit.upload({
-      file: file.buffer,
-      fileName: file.originalname,
-      folder: `/${folder}`,
-    });
-  
-    const url = `${details.url}#${details.fileId}`
-    console.log(url)
-    return url;
-    // SDK initialization
-  } catch (error) {
-    console.log(error.message);
-  }
+export const imageUploader = async (file, folder) => {
+    try {
+        const details = await imagekit.upload({
+            file: file.buffer,
+            fileName: file.originalname,
+            folder: `/${folder}`,
+        });
+
+        const url = `${details.url}#${details.fileId}`;
+        console.log(url);
+        return url;
+        // SDK initialization
+    } catch (error) {
+        console.log(error.message);
+    }
 };
