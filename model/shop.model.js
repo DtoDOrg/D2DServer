56c9daab5e5d6e5704612f4ab3498257da43516c
Jayanta <=>
2024-10-27 16:29:43 +0530

    shop model modified

--- model/shop.model.js
+++ model/shop.model.js
@@ -1,63 +1,61 @@
-import mongoose from "mongoose";
-import { Schema } from "mongoose";
-const ShopSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    owner: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    image: {
-      type: String,
-      required: true,
-    },
-    city: {
-      type: String,
-      required: true,
-    },
-    address: {
-      type: Object,
-      required: true,
-    },
-    phone: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    rating: {
-      type: Number,
-      default: 0,
-    },
+import mongoose from 'mongoose';
 
-    status: {
-      type: Boolean,
-      default: true,
+const shopSchema = new mongoose.Schema(
+    {
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'admin',
+            required: true,
+        },
+        category: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'category',
+            required: true,
+        },
+        shopName: {
+            type: String,
+            required: true,
+        },
+        shopImages: {
+            type: [String],
+            default: ['https://ik.imagekit.io/gvspmkmsw/y9DpT.jpg', 'https://ik.imagekit.io/gvspmkmsw/y9DpT.jpg', 'https://ik.imagekit.io/gvspmkmsw/y9DpT.jpg'],
+            validate: {
+                validator: function (value) {
+                    return value.length <= 5; // Set your maximum length here
+                },
+                message: 'You can only upload a maximum of 5 images.',
+            },
+        },
+        services: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'service',
+            },
+        ],
+        city: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'city',
+        },
+        state: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'state',
+        },
+        status: {
+            type: String,
+            enum: ['online', 'offline'],
+            default: 'online',
+        },
+        isActive: {
+            type: Boolean,
+            default: true,
+        },
     },
-    isDeleted: {
-      type: Boolean,
-      default: false,
-    },
-
-    category: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Category",
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-    versionKey: false,
-  }
+    {
+        timestamps: true,
+        versionKey: false,
+    }
 );
-const ShopModel = mongoose.model("Shop", ShopSchema);
+
+const ShopModel = mongoose.model('shop', shopSchema);
+
 export default ShopModel;
