f4371a1de8a1db25be7fee9a976ceff2b00343f8
Jayanta <=>
2024-12-05 23:27:19 +0530

    authorized admin

--- routes/admin/auth.admin.route.js
+++ routes/admin/auth.admin.route.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { create, deleteAdmin, getAll, getById, logIn } from '../../controller/admin.controller.js';
+import { CONFIG } from '../../config/config.js';
+import authorize from '../../middleware/authorization.middleware.js';
 const route = express.Router();
-route.get('/', getAll);
-route.get('/:id', getById);
+const role = CONFIG.ADMIN_ROLE;
 route.post('/login', logIn);
 route.post('/', create);
+route.use(authorize([role]));
+route.get('/', getAll);
+route.get('/:id', getById);
 route.delete('/:id', deleteAdmin);
 export { route as adminRouter };
